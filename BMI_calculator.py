5a20c6c6c7ed838eb5270f6d863c6241513c1329
Zocomon <sriparna1998@gmail.com>
2024-08-03 20:44:00 +0530

    futher changes in bmi calculation

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -2,4 +2,8 @@ weight = float(input("enter weight in KG: \n"))
 height = float(input("enter height in meter: \n"))
 
 bmi = weight / (height**2)
+rounded_bmi = round(bmi,2)
 print("your BMI: "+ str(bmi))
+print("your BMI: "+ str(round(bmi,2)))
+#f-string
+print(f"your BMI: {rounded_bmi}")
