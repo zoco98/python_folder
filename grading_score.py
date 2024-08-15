9f590932426df96ebe2396c328e46a044cf11b04
Zocomon <sriparna1998@gmail.com>
2024-08-15 18:36:33 +0530

    implemented review for grading

--- grading_score.py
+++ grading_score.py
@@ -8,11 +8,11 @@ student_scores = {
 print(student_scores)
 student_grades = student_scores
 for key in student_scores:
-    if student_scores[key]>91 and student_scores[key]<100:
+    if student_scores[key]>=91 and student_scores[key]<=100:
         student_grades[key] = "Outstanding"
-    elif student_scores[key]>81 and student_scores[key]<90:
+    elif student_scores[key]>=81 and student_scores[key]<=90:
         student_grades[key] = "Exceeds Expectations"
-    elif student_scores[key]>71 and student_scores[key]<80:
+    elif student_scores[key]>=71 and student_scores[key]<=80:
         student_grades[key] = "Acceptable"
     else:
         student_grades[key] = "Fail"
