174e37a431f673a0beecdef5ef3c30dc5c922d34
Zocomon <sriparna1998@gmail.com>
2024-08-15 16:35:11 +0530

    silent bidding game

---
+++ silent_bid_game.py
@@ -0,0 +1,34 @@
+import os
+#taking inputs
+your_name = input("what's your name: \n")
+start_bidding_amount = int(input("How much you want to bid: \n"))
+
+#declare var
+bid_dict = {}
+max_bid = 0
+winner = ""
+bid_dict[your_name] = start_bidding_amount
+
+auction_closed = False
+while not auction_closed:
+    #taking inputs
+    bidder_count = input("is there anymore bidder? yes/no: \n")
+    if bidder_count == "yes":
+        #print("\n"*100)
+        os.system('cls')
+        bidder_name = input("what's bidder name: \n")
+        bidder_amount = int(input("How much you want to bid: \n"))
+        bid_dict[bidder_name] = bidder_amount
+    else:
+        auction_closed = True
+
+#print(bid_dict)
+os.system('cls')
+print("\n*************winner*************\n")
+for key in bid_dict:
+    if bid_dict[key]>max_bid:
+        winner = key
+        max_bid = bid_dict[key]
+print(f"Winner is {winner} who bid for {max_bid}")
+print("\n*************winner*************\n")
+
