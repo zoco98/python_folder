387436adc3ee581947cf0ecd3e9af6ca3460a503
Zocomon <sriparna1998@gmail.com>
2024-08-15 16:42:14 +0530

    implemented review silent bid

--- silent_bid_game.py
+++ silent_bid_game.py
@@ -14,6 +14,7 @@ while not auction_closed:
     #taking inputs
     bidder_count = input("is there anymore bidder? yes/no: \n")
     if bidder_count == "yes":
+        #clear console after each bid
         #print("\n"*100)
         os.system('cls')
         bidder_name = input("what's bidder name: \n")
@@ -25,10 +26,18 @@ while not auction_closed:
 #print(bid_dict)
 os.system('cls')
 print("\n*************winner*************\n")
+print("method-1: looping with max var")
 for key in bid_dict:
     if bid_dict[key]>max_bid:
         winner = key
         max_bid = bid_dict[key]
 print(f"Winner is {winner} who bid for {max_bid}")
 print("\n*************winner*************\n")
+
+print("\n*************winner*************\n")
+print("method-2: using max function")
+max(bid_dict,key=bid_dict.get)
+print(f"Winner is {winner} who bid for {max_bid}")
+print("\n*************winner*************\n")
+
 
